1bd2bcfb0f83c84094dad3fe11d111c4024b0cf1
vagabond <athomec@pie.com.tw>
2023-08-07 15:07:48 +0800

    上傳檔案

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+$(function () {//JS開頭
+
+	var WINDOW = $(window).width();//視窗寬度
+	var WINDOWH = $(window).height();//視窗高度
+
+	//字級大小
+	$(".js-top-function button").click(function () {
+		if ($(this).hasClass("js-font-sm")) {
+			$("html").addClass("sm");
+		} else if ($(this).hasClass("js-font-md")) {
+			$("html").removeClass("sm").removeClass("lg");
+		} else if ($(this).hasClass("js-font-lg")) {
+			$("html").addClass("lg");
+		}
+		$(this).addClass("active");
+		return false;
+	})
+	
+})//JS尾端	
